d171c95bdcdd8f4c44cf431d7d5523a618d7f266
xzorphit <52004595+xzorphit@users.noreply.github.com>
2020-01-17 14:30:59 -0500

    sweep and replace

--- WEBPRAC/test/pages/GettingStartedPage.js
+++ WEBPRAC/test/pages/GettingStartedPage.js
@@ -9,13 +9,24 @@ export default class GettingStartedPage extends BasePage {
 
             this.gettingStartedPageTitle = 'Getting Started · WebdriverIO';
 
+            this.gsSearch ='#search_input_react';
+
             this.nodejsLink = '/html/body/div[2]/div/div[2]/div/div[1]/article/div/span/p[3]/a[1]';
+
+            this.gsfindElementTitle = '#findelement';
         }
 
         // public variables
 
+        get gsSearchField() {
+            return browser.$(this.gsSearch);
+        }
+        
         get homepageNodeJSLink() {
             return browser.$(this.nodejsLink);
+        }
+        get findElementTItle(){
+            return browser.$(this.gsfindElementTitle);
         }
 
         //public functions
